be77ceb8632423467234a88735d5aad0fc09cd7c
Bruno Martins <bgcngm@gmail.com>
2025-04-30 21:12:33 +0100

    venus: Set camera override format from reserved via soong config
    Change-Id: I6a089e783f26c4515940d74f8e912dffa2789550
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -9,6 +9,7 @@ $(call inherit-product, device/xiaomi/sm8350-common/common.mk)
 
 # Inherit from miuicamera-venus
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-venus/device.mk)
+$(call soong_config_set,camera,override_format_from_reserved,true)
 
 # Fingerprint
 PRODUCT_PACKAGES += \
